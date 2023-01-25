68375220deb7cca19e48ec80cc2b258b77d55ee4
bfalad22 <bolasaliu93@gmail.com>
2023-01-25 13:29:35 -0800

    printf

--- main.h
+++ main.h
@@ -58,5 +58,4 @@ char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
 
-
 #endif
